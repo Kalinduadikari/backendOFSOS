b93c38e6126ed470fa197e07a6d098f29a3b476c
Kalinduadikari <kalindukadikari@gmail.com>
2023-07-09 16:41:01 +0530

    changed the authjs file a bit

--- Controllers/auth.js
+++ Controllers/auth.js
@@ -37,7 +37,12 @@ export const requireSignin = expressjwt({
 // authenticateUser middleware
 export const authenticateUser = async (req, res, next) => {
   try {
-    const token = req.headers.authorization.split(" ")[1];
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(401).json({ error: "Unauthorized, no token found" });
+    }
+
+    const token = authHeader.split(" ")[1];
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
     const user = await User.findById(decoded._id);
