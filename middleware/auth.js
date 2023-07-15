e7ad9c92e8f35ea04011a9ee058fad6ad29302ad
Kalinduadikari <kalindukadikari@gmail.com>
2023-07-15 09:02:17 +0530

    did some changes to the auth middlware again

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,36 +1,62 @@
-import jwt from "jsonwebtoken";
-import Fishmonger from "../Models/fishmongers";
-import CustomError from "../errors/CustomError";
-require('dotenv').config();
+const jwt = require("jsonwebtoken");
+const Fishmonger = require("../Models/fishmongers");
+const CustomError = require("../errors/CustomError");
+require("dotenv").config();
 
-export const protect = async (req, res, next) => {
+const protect = async (req, res, next) => {
   try {
     let token;
 
-    if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+    if (
+      req.headers &&
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer")
+    ) {
       token = req.headers.authorization.split(" ")[1];
     } else if (req.cookies && req.cookies.token) {
       token = req.cookies.token;
     }
 
     if (!token) {
-      throw new CustomError(401, "Not authorized, no token found", "NO_TOKEN_FOUND", "No token found in the request headers or cookies.", new Error().stack);
+      throw new CustomError(
+        401,
+        "Not authorized, no token found",
+        "NO_TOKEN_FOUND",
+        "No token found in the request headers or cookies.",
+        new Error().stack
+      );
     }
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const fishmonger = await Fishmonger.findById(decoded.id).select("-password");
 
     if (!fishmonger) {
-      throw new CustomError(404, "No fishmonger found with this ID", "FISHMONGER_NOT_FOUND", `Fishmonger with ID ${decoded.id} not found.`, new Error().stack);
+      throw new CustomError(
+        404,
+        "No fishmonger found with this ID",
+        "FISHMONGER_NOT_FOUND",
+        `Fishmonger with ID ${decoded.id} not found.`,
+        new Error().stack
+      );
     }
 
     req.fishmonger = fishmonger;
     next();
   } catch (error) {
     if (error instanceof jwt.JsonWebTokenError) {
-      next(new CustomError(401, "Not authorized, invalid token", "INVALID_TOKEN", "The provided token is invalid or expired.", new Error().stack));
+      next(
+        new CustomError(
+          401,
+          "Not authorized, invalid token",
+          "INVALID_TOKEN",
+          "The provided token is invalid or expired.",
+          new Error().stack
+        )
+      );
     } else {
       next(error);
     }
   }
 };
+
+module.exports = protect;
