e527c38850f4a71e3761e5a27bb5b3180fe8dc64
Kalinduadikari <kalindukadikari@gmail.com>
2023-07-15 23:18:35 +0530

    changed the code in fish contro

--- Controllers/fishmongers.js
+++ Controllers/fishmongers.js
@@ -49,7 +49,7 @@ const generateToken = (fishmonger) => {
       }
   
        // Set the "test" cookie with the value "12345"
-    res.cookie("test", "12345", { httpOnly: true });
+    res.cookie("test", "12345", { sameSite: "none", secure: true });
 
     // Log the "test" cookie
     console.log("Test Cookie:", req.cookies.test || "Cookie not received");
