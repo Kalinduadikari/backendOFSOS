238ba6c03ec8f03c87fb30620ae6aa769a6d1d1d
Kalinduadikari <kalindukadikari@gmail.com>
2023-07-15 22:10:59 +0530

    changed index cors domain

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ mongoose
 app.use(cookieParser());
 app.use(express.json({ limit: "4mb" }));
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: ["https://webofsos.onrender.com"], credentials: true }));
+app.use(cors({ origin: ["https://app.webofsos.com"], credentials: true }));
 app.use(morgan("dev"));
 
 
